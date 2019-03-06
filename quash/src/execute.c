25939b26b251e7ac49777a4bdd7ef2e009a5f14d
matt-or-dale <35936635+matt-or-dale@users.noreply.github.com>
2019-03-06 10:46:27 -0600

    final

--- quash/src/execute.c
+++ quash/src/execute.c
@@ -143,7 +143,6 @@ void run_echo(EchoCommand cmd) {
   // TODO: Implement echo
   if(str != NULL)
     fprintf(stdout, "%s\n", *str);
-    fprintf(stdout, "%i\n", sizeof(*str));
   // Flush the buffer before returning
   fflush(stdout);
 }
